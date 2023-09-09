2555f9d291bccf00c59b1b54ab16f077de35baf5
Hunter <hunter@citrus.fund>
2023-09-09 11:57:37 -0400

    Initial Commit

---
+++ DefiLlama/DefiLlamaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DefiLlamaApp.swift
+//  DefiLlama
+//
+//  Created by Hunter Diamond on 9/9/23.
+//
+
+import SwiftUI
+
+@main
+struct DefiLlamaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
